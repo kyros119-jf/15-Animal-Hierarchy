43d2b3ea760124e5043b5af2df73e623bf2e7692
Jeremy <jeremy.frank@students.snow.edu>
2026-09-18 06:53:02 -0600

    Completed aniaml hierarchy assignment

---
+++ Animal.cs
@@ -0,0 +1,39 @@
+﻿public class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+
+
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is Sleeping");
+    }
+
+}
+
+public class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog Barks");
+    }
+}
+
+public class Cat : Animal
+{
+
+    public void Meow()
+    {
+        Console.WriteLine("Cat Meows");
+    }
+}
+
+public class Bird : Animal
+{
+    public void Fly()
+    {
+        Console.WriteLine("Bird Flies");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Dog dog = new Dog();
+
+dog.Bark();
+dog.Eat();
+dog.Sleep();
+
+Console.WriteLine();
+
+Cat cat = new Cat();
+
+cat.Meow();
+cat.Sleep();
+cat.Eat();
+
+Console.WriteLine();
+
+Bird bird = new Bird();
+
+bird.Sleep();
+bird.Eat();
+bird.Fly();
